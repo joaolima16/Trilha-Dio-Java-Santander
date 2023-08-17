9f4eeb29bc3b510043c3a320047d598281b68390
joaolima16 <joaovitordlg23101@gmail.com>
2023-08-17 10:36:09 -0300

    exercicio-corrigido

--- src/ExercicioContador/App.java
+++ src/ExercicioContador/App.java
@@ -1,15 +1,20 @@
 package ExercicioContador;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        try{
-            Contador  cont = new Contador();
-            cont.contar(10, 15);
-        }
-        catch(ContadorException e){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite o numero inicial da contagem");
+            int numInical = sc.nextInt();
+            System.out.println("Digite o numero final da contagem");
+            int numFinal = sc.nextInt();
+            Contador cont = new Contador();
+            cont.contar(numInical, numFinal);
+        } catch (ContadorException e) {
             System.out.println("Ocorreu um erro executar a função");
         }
-        
-       
+
     }
 }
